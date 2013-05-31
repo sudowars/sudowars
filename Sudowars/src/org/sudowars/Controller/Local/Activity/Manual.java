4bc404b63677bd64c6d281416e68bd31f8837b67
Moritz Lüdecke <ritze@skweez.net>
2013-05-31 15:38:15 +0200

    Add PoolBinder to Manual Activity (dirty c&p)

--- Sudowars/src/org/sudowars/Controller/Local/Activity/Manual.java
+++ Sudowars/src/org/sudowars/Controller/Local/Activity/Manual.java
@@ -47,11 +47,18 @@
 package org.sudowars.Controller.Local.Activity;
 
 import org.sudowars.R;
+import org.sudowars.Model.SudokuManagement.Pool.SudokuFilePool;
+import org.sudowars.Model.SudokuManagement.Pool.SudokuPool;
+import org.sudowars.Model.SudokuManagement.Pool.SudokuFilePool.SudokuFilePoolBinder;
 
 import android.app.ActionBar;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -66,8 +73,69 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-//TODO: extends PoolBinder
-public class Manual extends FragmentActivity {        
+public class Manual extends FragmentActivity {
+	/**
+	 * the pool full of Sudokus
+	 */
+	protected SudokuPool pool;
+	
+	/**
+	 * tricker, if this activity is bound to the pool
+	 */
+	private boolean bound = false;
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onStart()
+	 */
+	@Override
+    protected void onStart() {
+        super.onStart();
+        
+        Intent intent = new Intent(this, SudokuFilePool.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onStop()
+	 */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        
+        if (this.bound) {
+            unbindService(connection);
+            this.bound = false;
+        }
+    }
+    
+    /*
+     * Defines callbacks for service binding, passed to bindService()
+     */
+    private ServiceConnection connection = new ServiceConnection() {
+    	/*
+    	 * (non-Javadoc)
+    	 * @see android.content.ServiceConnection#onServiceConnected(android.content.ComponentName, android.os.IBinder)
+    	 */
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            //we've bound to SudokuFilePool, cast the IBinder and get SudokuFilePool instance
+            SudokuFilePoolBinder binder = (SudokuFilePoolBinder) service;
+            Manual.this.pool = binder.getService();
+            Manual.this.bound = true;
+        }
+        
+        /*
+         * (non-Javadoc)
+         * @see android.content.ServiceConnection#onServiceDisconnected(android.content.ComponentName)
+         */
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+        	Manual.this.bound = false;
+        }
+    };
+    
 	/**
      * The {@link android.support.v4.view.PagerAdapter} that will provide fragments representing
      * each object in a collection.
